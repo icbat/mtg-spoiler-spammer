189ed55f3b6e959f07ef3950ea1a8de56e9ccebc
Evan Cobb <icbat@pm.me>
2019-06-22 19:57:14 -0500

    messed up a merge somewhere...

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 const cards = require('./cards')
 const slack = require('./slack')
 
-<<<<<<< HEAD
-const getNewCardsUrl = "https://api.scryfall.com/cards?order=spoiled"
-
-const sendToSlackUrl = process.env.SLACK_URL
-
-const parseResponse = resp => {
-    console.log('response resceived')
-    const body = JSON.parse(resp.body)
-
-    console.log(body.data[0])
-
-    const trimmed = body.data
-        .map(trimToRelevantFields)
-
-
-        console.log(trimmed[0])
-
-        return trimmed
-}
-
-const trimToRelevantFields = card => {
-    const { set_name, name, scryfall_uri, set_uri, image_uris: { large } } = card
-    return { set_name, name, card_uri: scryfall_uri, set_uri, picture: large }
-}
-=======
->>>>>>> 4bd5df4... extracting some files to keep it clean
 
 const limitResponse = n => cards => cards.slice(0, n)
 
