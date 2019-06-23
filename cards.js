e4c3cbaf1232bd365bb2cb575ce38da42d847cc7
Evan Cobb <icbat@pm.me>
2019-06-22 19:48:49 -0500

    smaller pictures

--- cards.js
+++ cards.js
@@ -31,8 +31,8 @@ const parseResponse = resp => {
 }
 
 const trimToRelevantFields = card => {
-    const { set_name, name, scryfall_uri, set_uri, image_uris: { large } } = card
-    return { set_name, name, card_uri: scryfall_uri, set_uri, picture: large }
+    const { set_name, name, scryfall_uri, set_uri, image_uris: { normal } } = card
+    return { set_name, name, card_uri: scryfall_uri, set_uri, picture: normal }
 }
 
 module.exports = {
