127f006cae70edbac2502f3ef1a5b2c2df00dc20
Evan Cobb <icbat@pm.me>
2019-06-22 22:56:13 -0500

    stopping sending messages if we've seen it before

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const slack = require('./lib/slack')
 const db = require('./lib/db')
 
 
-const limitResponse = n => cards => cards.slice(0, n)
-
 const handleError = error => {
     console.log(error)
 }
@@ -12,13 +10,7 @@ const handleError = error => {
 cards.getCards()
     .then(cards.parseResponse)
     .then(cardList => cardList.map(cards.trimToRelevantFields))
-    .then(limitResponse(3))
-    // TODO filter out the ones we've already sent
-    .then(cards => {
-        const lastSeen = db.setLastSeenCard(cards[0].name)
-        console.log('last seen', lastSeen)
-        return cards
-    })
+    .then(db.filterSeenCards)
     // TODO .then( if there's more than N, consolidate)
     .then(slack.createMessage)
     .then(slack.sendToChat)
