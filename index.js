6ad2184d2fff49848afe2eeaa0e3c201b7953507
Evan Cobb <icbat@pm.me>
2019-06-22 23:26:24 -0500

    limiting the worst-case we send back anyway

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const handleError = error => {
     console.log(error)
 }
 
+const truncateCards = cards => {
+    const length = Math.min(cards.length, 5)
+
+    return cards.slice(0, length)
+}
+
 cards.getCards()
     .then(cards.parseResponse)
     .then(cardList => cardList.map(cards.trimToRelevantFields))
     .then(db.filterSeenCards)
-    // TODO .then( if there's more than N, consolidate)
+    .then(truncateCards)
     .then(slack.createMessage)
     .then(slack.sendToChat)
     .catch(handleError)
