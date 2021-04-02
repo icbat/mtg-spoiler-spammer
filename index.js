2dd1b324a03ba6ba762d20e1735749458c8e08bd
EvanCobb <evan.cobb@protonmail.com>
2021-04-02 12:52:57 -0500

    adds eslint w/ standard js and lints everything

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const cards = require('./lib/cards')
 const slack = require('./lib/slack')
 const db = require('./lib/db')
 
-
 const handleError = error => {
-    console.log(error)
+  console.log(error)
 }
 
 const truncateCards = cards => {
-    const length = Math.min(cards.length, 5)
+  const length = Math.min(cards.length, 5)
 
-    return cards.slice(0, length)
+  return cards.slice(0, length)
 }
 
 cards.getCards()
-    .then(cards.parseResponse)
-    .then(cardList => cardList.map(cards.trimToRelevantFields))
-    .then(db.filterSeenCards)
-    .then(truncateCards)
-    .then(slack.createMessage)
-    .then(slack.sendToChat)
-    .catch(handleError)
+  .then(cards.parseResponse)
+  .then(cardList => cardList.map(cards.trimToRelevantFields))
+  .then(db.filterSeenCards)
+  .then(truncateCards)
+  .then(slack.createMessage)
+  .then(slack.sendToChat)
+  .catch(handleError)
--- test/cards.spec.js
+++ test/cards.spec.js
@@ -6,42 +6,41 @@ const exampleCardDFC = require('./example-card-doublefaced.json')
 jest.mock('got')
 
 describe('trimToRelevantFields', () => {
-    const expectedKeys = ['card_uri', 'name', 'picture', 'set_name', 'set_uri']
+  const expectedKeys = ['card_uri', 'name', 'picture', 'set_name', 'set_uri']
 
-    test('trims a normal card', () => {
-        const result = cards.trimToRelevantFields(exampleCardNormal)
-    
-        expect(result).toBeTruthy()
-        expect(Object.keys(result).length).toEqual(expectedKeys.length)
-    
-        for (const key of expectedKeys) {
-            expect(Object.keys(result)).toContain(key)
-            expect(result[key]).toEqual(expect.any(String))
-        }
-    })
+  test('trims a normal card', () => {
+    const result = cards.trimToRelevantFields(exampleCardNormal)
 
-    test('uses the "normal" image on a normal card', () => {
-        const result = cards.trimToRelevantFields(exampleCardNormal)
+    expect(result).toBeTruthy()
+    expect(Object.keys(result).length).toEqual(expectedKeys.length)
 
-        expect(result['picture']).toEqual('https://c1.scryfall.com/file/scryfall-cards/normal/front/5/1/51464df6-557f-47e5-838e-2a30145511f0.jpg?1576382174')
-    })
-    
-    test('trims a double-faced card', () => {
-        const result = cards.trimToRelevantFields(exampleCardDFC)
-    
-        expect(result).toBeTruthy()
-        expect(Object.keys(result).length).toEqual(expectedKeys.length)
-    
-        for (const key of expectedKeys) {
-            expect(Object.keys(result)).toContain(key)
-            expect(result[key]).toEqual(expect.any(String))
-        }
-    })
-    
-    test('uses the "normal" image on a double-faced card', () => {
-        const result = cards.trimToRelevantFields(exampleCardDFC)
+    for (const key of expectedKeys) {
+      expect(Object.keys(result)).toContain(key)
+      expect(result[key]).toEqual(expect.any(String))
+    }
+  })
 
-        expect(result['picture']).toEqual('https://c1.scryfall.com/file/scryfall-cards/normal/front/0/2/028aeebc-4073-4595-94da-02f9f96ea148.jpg?1562825445')
-    })
+  test('uses the "normal" image on a normal card', () => {
+    const result = cards.trimToRelevantFields(exampleCardNormal)
 
+    expect(result.picture).toEqual('https://c1.scryfall.com/file/scryfall-cards/normal/front/5/1/51464df6-557f-47e5-838e-2a30145511f0.jpg?1576382174')
+  })
+
+  test('trims a double-faced card', () => {
+    const result = cards.trimToRelevantFields(exampleCardDFC)
+
+    expect(result).toBeTruthy()
+    expect(Object.keys(result).length).toEqual(expectedKeys.length)
+
+    for (const key of expectedKeys) {
+      expect(Object.keys(result)).toContain(key)
+      expect(result[key]).toEqual(expect.any(String))
+    }
+  })
+
+  test('uses the "normal" image on a double-faced card', () => {
+    const result = cards.trimToRelevantFields(exampleCardDFC)
+
+    expect(result.picture).toEqual('https://c1.scryfall.com/file/scryfall-cards/normal/front/0/2/028aeebc-4073-4595-94da-02f9f96ea148.jpg?1562825445')
+  })
 })
