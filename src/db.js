001125a2c0bf40df11da96c7b8abae6e0437fb0c
EvanCobb <evan.cobb@protonmail.com>
2021-09-06 11:28:00 -0500

    adds a get and adds get and set methods to db
    so we can do some set lookup w/o knowing if the value is there or not

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const getset = (key, newValue) => {
   redis.auth(password)
 
   return new Promise((resolve, reject) => {
-    console.log('Checking the cache', newValue)
+    console.log('Checking the cache', key, newValue)
 
     redis.getset(key, newValue, (error, result) => {
       if (error) {
@@ -21,6 +21,54 @@ const getset = (key, newValue) => {
   })
 }
 
+const get = (key) => {
+  const redisURL = require('./env').redis
+  // https://devcenter.heroku.com/articles/redistogo#using-with-node-js
+  const { URL } = require('url')
+  const { port, hostname, password } = new URL(redisURL)
+  const redis = require('redis').createClient(port, hostname)
+
+  redis.auth(password)
+
+  return new Promise((resolve, reject) => {
+    console.log('Checking the cache', key)
+
+    redis.get(key, (error, result) => {
+      if (error) {
+        return reject(error)
+      }
+
+      console.log('found', result, error)
+      return resolve(result)
+    })
+  })
+}
+
+const set = (key, newValue) => {
+  const redisURL = require('./env').redis
+  // https://devcenter.heroku.com/articles/redistogo#using-with-node-js
+  const { URL } = require('url')
+  const { port, hostname, password } = new URL(redisURL)
+  const redis = require('redis').createClient(port, hostname)
+
+  redis.auth(password)
+
+  return new Promise((resolve, reject) => {
+    console.log('Setting new value', key, newValue)
+
+    redis.set(key, newValue, (error, result) => {
+      if (error) {
+        return reject(error)
+      }
+
+      console.log('found', result, error)
+      return resolve(result)
+    })
+  })
+}
+
 module.exports = {
   getset,
+  get,
+  set,
 }
